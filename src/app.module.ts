eb5ee9574d740bfa614ec5a7691c5231b6098ac0
Manuel <3057476220301@ingenieria.usac.edu.gt>
2025-01-29 21:49:09 -0600

    added cross-env for using different db for testing and development

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,14 +17,25 @@ const cookieSession = require('cookie-session');
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      // NOTE: WE CAN USE THIS LINE, BUT THIS IS NOT THE NEST RECOMMENDED WAY.
-      // database: process.env.NODE_ENV === 'test' ? 'test.sqlite' : 'db.sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          entities: [User, Report],
+          synchronize: true,
+        };
+      },
     }),
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   // NOTE: WE CAN USE THIS LINE, BUT THIS IS NOT THE NEST RECOMMENDED WAY.
+    //   // database: process.env.NODE_ENV === 'test' ? 'test.sqlite' : 'db.sqlite',
+    //   database: 'db.sqlite',
+    //   entities: [User, Report],
+    //   synchronize: true,
+    // }),
     UsersModule,
     ReportsModule,
   ],
